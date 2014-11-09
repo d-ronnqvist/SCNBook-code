d3bcc3ae3a71a826757b206e112c2ef6fbd89a78
David Rönnqvist <david.ronnqvist@gmail.com>
2014-11-09 18:26:22 +0100

    First version of Chapter 3 (Project 2)

---
+++ Chapter 03 - Pie Chart/Chapter 03 - Pie Chart/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Chapter 03 - Pie Chart
+//
+//  Created by David Rönnqvist on 08/06/14.
+//  Copyright (c) 2014 David Rönnqvist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
